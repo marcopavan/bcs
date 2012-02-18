6769b693edc2f5ebbd6588deb74958c14caf3e2d
andreapavan <andreapavan89@gmail.com>
2012-02-18 23:30:02 +0100

    jcarousel added

--- index.php
+++ index.php
@@ -3,6 +3,25 @@
 <head>
 <title>The New Bottol Creation System</title>
 <link rel="stylesheet" href="style.css" type="text/css" media="screen, print" />
+<script type="text/javascript" language="javascript" src="js/jquery-1.7.1.min.js">
+	
+</script>
+<script type="text/javascript" language="javascript" src="js/jquery.carouFredSel-5.5.0-packed.js">
+	
+</script>
+<script type="text/javascript" language="javascript">
+	$(function() {
+		$('#elements').carouFredSel({
+			circular: false,
+			auto: false,
+			infinite: false,
+			scroll: {items: "page"},
+			prev: {button: "#elements_prev", key: "left"},
+			next: {button: "#elements_next", key: "right"},
+			pagination: "#elements_pag"
+		});
+	});
+</script>
 </head>
 
 <body>
@@ -19,6 +38,50 @@
 			<input type="text" name="topic_title" id="topic_title" value="" autocomplete="off" maxlength="50" /><span class="asterisco">*</span><br/>			
 			<label>Subtitle:</label><br/>
 			<input type="text" name="topic_subtitle" id="topic_subtitle" value="" autocomplete="off" maxlength="80" /><!-- <span class="asterisco">*</span> --><br/>
+
+			<!-- Start jcarousel -->
+
+			<div class="list_carousel">
+				<div id="elements">
+					<div class="element">
+						<img src="img/document.png"/>
+					</div>
+					<div class="element">
+						<img src="img/image.png"/>
+					</div>
+					<div class="element">
+						<img src="img/movie.png"/>
+					</div>
+					<div class="element">
+						<img src="img/music.png"/>
+					</div>
+					<div class="element">
+						<img src="img/picture.png"/>
+					</div>
+					<div class="element">
+						<img src="img/text.png"/>
+					</div>
+					<div class="element">
+						<img src="img/video.png"/>
+					</div>
+					<div class="element">
+						<img src="img/picture.png"/>
+					</div>
+					<div class="element">
+						<img src="img/video.png"/>
+					</div>
+					<div class="element">
+						<img src="img/music.png"/>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+				<a class="prev" id="elements_prev" href="#"><span>&lt</span></a>
+				<a class="next" id="elements_next" href="#"><span>&gt</span></a>
+				<div class="pagination" id="elements_pag"></div>
+			</div>
+
+			<!-- End jcarousel -->
+
 			<input type="submit" name="submit_bottol" id="submit_bottol" value="Create"/>
 			<input type="button" id="save_bottol" value="Save as Draft"/>
 			<input type="button" id="preview_bottol" value="Preview"/>
