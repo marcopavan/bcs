d76d66a24a6bc267f9f1958aaca97613f487c504
Giovanni Muzzolini <giovannimuzzolini@MacBook-di-Giovanni-Muzzolini.local>
2012-02-27 16:38:13 +0100

    document uploader optimized

--- js/uploader.js
+++ js/uploader.js
@@ -78,58 +78,26 @@
         }
       
 
-        if(!window.FileReader){
-          var form = new FormData();
-          form.append('file', f);
-          $.ajax({
-              url: 'getBase64.php',
-              data: form,
-              cache: false,
-              contentType: false,
-              processData: false,
-              type: 'POST',
-              beforeSend: function(){
-                //actual_drop_zone.find('.message').html(spinner);
-                actual_drop_zone.html(spinner);
-                actual_drop_zone.html(spinner + '<div class="percentLoaded"></div>');
-                actual_drop_zone.find('.percentLoaded').html('Creating preview...');
-              },
-              success: function(data){
-                $.post('tmp/saveTemp.php', {base64: data, name: f.name}, function(urlToGDocs){
-                  actual_drop_zone.html('<div class="dropped_div"><iframe id="document_frame" src="http://docs.google.com/gview?url='+escape(urlToGDocs)+'&embedded=true" style="width:100%; height:600px;" frameborder="0"></iframe></div>');
-                  actual_drop_zone.addClass('img_added');
-                });
-              }
-          });
-          break;
-        }
-        var reader = new FileReader();
-        //shows a spinner
-        reader.onloadstart = function(e) {
-          //actual_drop_zone.find('.message').html(spinner + '<div class="percentLoaded"></div>');
-          actual_drop_zone.html(spinner + '<div class="percentLoaded"></div>');
-        };
-        //change percentage
-        reader.onprogress = function(e) {
-          actual_drop_zone.find('.percentLoaded').html(Math.round((e.loaded / e.total) * 100) + '%');
-        };
-        //loads file and save for googledocs
-        reader.onload = (function(theFile) {
-          return function(e) {
-            // Render dropped image.
-            var dropArea = actual_drop_zone;
-            var base64_string = e.target.result;
-            base64_string = base64_string.substring(base64_string.indexOf('base64,') + 7);
-            actual_drop_zone.find('.percentLoaded').html('Creating preview...');
-            $.post('tmp/saveTemp.php', {base64: base64_string, name: theFile.name}, function(urlToGDocs){
-              dropArea.html('<div class="dropped_div"><iframe id="document_frame" src="http://docs.google.com/gview?url='+escape(urlToGDocs)+'&embedded=true" style="width:100%; height:600px;" frameborder="0"></iframe></div>');
+        var form = new FormData();
+        form.append('file', f);
+        form.append('name', f.name);
+        $.ajax({
+            url: 'tmp/saveTemp.php',
+            data: form,
+            cache: false,
+            contentType: false,
+            processData: false,
+            type: 'POST',
+            beforeSend: function(){
+              actual_drop_zone.html(spinner);
+              actual_drop_zone.html(spinner + '<div class="percentLoaded"></div>');
+              actual_drop_zone.find('.percentLoaded').html('Creating preview...');
+            },
+            success: function(urlToGDocs){
+              actual_drop_zone.html('<div class="dropped_div"><iframe id="document_frame" src="http://docs.google.com/gview?url='+escape(urlToGDocs)+'&embedded=true" style="width:100%; height:600px;" frameborder="0"></iframe></div>');
               actual_drop_zone.addClass('img_added');
-            });         
-          };
-        })(f);
-
-        // Read in the image file as a data URL.
-        reader.readAsDataURL(f);
+            }
+        });
       }
     }
   }
--- tmp/saveTemp.php
+++ tmp/saveTemp.php
@@ -1,11 +1,10 @@
 <?php
 	$name = $_POST['name'];
-	$base64 = $_POST['base64'];
 
 	while(file_exists($name))
 		$name = rand().$name;
 	
-	file_put_contents($name, base64_decode($base64));
+	move_uploaded_file($_FILES['file']['tmp_name'], $name);
 
 	echo 'http://'.$_SERVER['SERVER_NAME'].str_replace('saveTemp.php', '', $_SERVER['PHP_SELF']).$name;
 ?>
