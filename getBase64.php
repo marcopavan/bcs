36771222ec910d1c1cd5faf9171636a2187a6b98
andreapavan <andreapavan89@gmail.com>
2012-03-11 16:26:53 +0100

    elements' border fixed

--- getBase64.php
+++ getBase64.php
@@ -1,3 +1,4 @@
 <?php
 	echo base64_encode(file_get_contents($_FILES['file']['tmp_name']));
+	exit
 ?>
--- show.php
+++ show.php
@@ -6,9 +6,7 @@
 <link rel="stylesheet" href="js/fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="screen" />
 <script type="text/javascript" language="javascript" src="js/jquery-1.7.1.min.js"></script>
 <script type="text/javascript" src="js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-
 <script type="text/javascript" src="js/jquery-ui-1.8.16.custom.min.js"></script>
-<script type="text/javascript" src="js/blocks.js"></script>
 
 <script type="text/javascript">
 	$(document).ready(function() {
